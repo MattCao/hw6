5024d7eddc88de3065ac68cb93f9900d919fca6d
Cao Yang <caoyangkaka@mattmac.att.net>
2013-03-12 02:02:48 -0700

    updated v1.0

--- encoding.pl
+++ encoding.pl
@@ -1,11 +1,43 @@
 #!/usr/usc/bin/perl
+
 $size_of_form_info = $ENV{'CONTENT_LENGTH'};
 read(STDIN, $form_info, $size_of_form_info); 
 $form_info =~ s/%([\dA-Fa-f][\dA-Fa-f])/pack ("C", hex ($1))/eg;
 #s is substitute, \dA-Fa-f looks for hex number and stores it in $1
 #pack and hex convert the value in $1 to ASCII, e evaluates second part 
 #of the substitute command as an expression, g replaces all occurrences
-($field_name, $data) = split (/=/, $form_info);
-print "Content-type: text/plain", "\n\n";
-print "Your input is: $data, right?", "\n"; 
+($search_data, $type_data) = split (/&/, $form_info);
+($itemA, $input) = split (/=/, $search_data);
+($itemB, $kind) = split (/=/, $type_data);
+print "Content-type: text/plain/selection", "\n\n";
+print "Your $itemA is: $input, right?", "\n";
+print "Your $itemB is: $kind, right?", "\n";
+$kind = lc($kind);
+use LWP::Simple;
+use URI;
+my $rel_url = "search/".$kind."/".$input;
+my $url = URI->new('http://www.allmusic.com/');
+$url->path($rel_url);
+print "The URL is now: $url\n";
+
+$content = LWP::Simple::get($url);
+#content handling
+@contents = split(/\n/, $content);
+my $flag = 0;
+my @result_url = {};
+for ($i = 0; $i <= $#contents; $i++) {
+	if($contents[$i] =~ /div class=\"name\"/) {
+		$flag = 1;
+		next;
+	}
+	if($flag) {
+	    if ($contents[$i] =~ /(http[^\"]*)/) {
+	        print $1."\n";
+		    push(@result_url, $1);
+		    
+	    }
+		$flag = 0;
+		
+	}
+}
 exit(0);
